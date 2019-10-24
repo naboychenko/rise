9cddaac93b04ca42886a05bbfbf016975dfe8d4b
Виктор Сучилин <viktr@gmail.com>
2019-10-24 18:29:09 +0300

    [NF] RISE-19 Initial config

---
+++ demo/src/test/java/ru/mentoring/rise/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mentoring.rise.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
